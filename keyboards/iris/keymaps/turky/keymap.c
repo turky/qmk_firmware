d3ec0589a8ea53f6e8d2a898bd3caf8d86091464
Akihiro Takizawa <akihiro.takizawa@gmail.com>
2018-01-25 01:33:08 +0900

    update

--- keyboards/iris/keymaps/turky/keymap.c
+++ keyboards/iris/keymaps/turky/keymap.c
@@ -1,27 +1,20 @@
 #include "iris.h"
 #include "action_layer.h"
 #include "eeconfig.h"
+#include "keymap_jp.h"
 
 extern keymap_config_t keymap_config;
 
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 16
+#define QWERTY 0
+#define LOWER 1
+#define RAISE 2
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
-};
+#define ____ KC_TRNS
+#define xxxx KC_NO
 
-#define KC_ KC_TRNS
-#define _______ KC_TRNS
-
-#define KC_QWE _QWERTY
-#define LOWR _LOWER
-#define KC_RASE _RAISE
+#define KC_QWE QWERTY
+#define KC_LOWR LOWER
+#define KC_RASE RAISE
 #define KC_RST RESET
 #define KC_BL_S BL_STEP
 
@@ -83,75 +76,65 @@ enum custom_keycodes {
 #define BTN2   KC_BTN2
 #define BTN1   KC_BTN1
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = KEYMAP(
+  [QWERTY] = KEYMAP(
   //,------+------+-----+-----+-----+-----.              ,----+----+----+----+----+----.
      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,        KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
   //|------+------+-----+-----+-----+-----|              |----+----+----+----+----+----|
      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,
   //|------+------+-----+-----+-----+-----|              |----+----+----+----+----+----|
-       CTL,  KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+     KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
   //|------+------+-----+-----+-----+-----+----.    ,----|----+----+----+----+----+----|
-       SFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_HENK,    KC_RGUI, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RO,
+     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_HENK,    KC_RGUI, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RO,
   //`------+------+-----+---+--+----+-----+----/    \----+----+----+----+----+----+----'
-                         KC_BSPC, KC_DEL, LT(_LOWER,KC_HOME),       LT(_RAISE,KC_END), KC_ENT ,KC_SPC
+                         KC_BSPC, KC_DEL, LT(KC_LOWR,KC_HOME),       LT(KC_RASE,KC_END),KC_ENT,KC_SPC
   //                    `------+----+----'        `----+----+----'
   ),
 
-  [_LOWER] = KC_KEYMAP(
+  [LOWER] = KEYMAP(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     TILD,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,BSPC,
+     TILD,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,KC_BSPC,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     RST , 1  , 2  , 3  , 4  , 5  ,                6  , 7  , 8  , 9  , 0  ,    ,
+     KC_RST, KC_1,KC_2,KC_3,KC_4,KC_5,            KC_6,KC_7,KC_8,KC_9,KC_0,____,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-     DEL ,    ,LEFT,RGHT, UP ,LBRC,               RBRC, P4 , P5 , P6 ,PLUS,PIPE,
+     KC_DEL,____,KC_LEFT,KC_RGHT,KC_UP,LBRC,    RBRC,KC_P4,KC_P5 ,KC_P6,PLUS,PIPE,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     BL_S,    ,    ,    ,DOWN,LCBR,LPRN,     RPRN,RCBR, P1 , P2 , P3 ,MINS,    ,
+   KC_BL_S,____,____,____,KC_DOWN,LCBR,LPRN, RPRN,RCBR, KC_P1,KC_P2,KC_P3,MINS,____,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           ,    ,DEL ,         DEL ,   , QWE
+                       ____,____,KC_DEL ,     KC_DEL ,____, TO(QWERTY)
   //                  `----+----+----'        `----+----+----'
   ),
 
-  [_RAISE] = KC_KEYMAP(
+  [RAISE] = KEYMAP(
   //,----+----+----+----+----+----.              ,----+----+----+----+----+----.
-     F12 , F1 , F2 , F3 , F4 , F5 ,                F6 , F7 , F8 , F9 ,F10 ,F11 ,
+     KC_F12,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5 ,      KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11 ,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,    ,
+     ____,EXLM, AT ,HASH,DLR ,PERC,               CIRC,AMPR,ASTR,LPRN,RPRN,____,
   //|----+----+----+----+----+----|              |----+----+----+----+----+----|
-         ,MPRV,MNXT,VOLU,PGUP,UNDS,               EQL ,HOME,    ,    ,    ,BSLS,
+     ____,KC_MPRV,KC_MNXT,VOLU,PGUP,UNDS,         EQL ,KC_HOME,____,____,____,BSLS,
   //|----+----+----+----+----+----+----.    ,----|----+----+----+----+----+----|
-     MUTE,MSTP,MPLY,VOLD,PGDN,MINS,    ,         ,PLUS,END ,    ,    ,    ,    ,
+     MUTE,____,____,VOLD,PGDN,MINS,____,     ____,PLUS,END ,____,____,____,____,
   //`----+----+----+--+-+----+----+----/    \----+----+----+----+----+----+----'
-                           ,    ,    ,             ,    ,QWE
+                       ____,____,____,         ____,____,TO(QWERTY)
   //                  `----+----+----'        `----+----+----'
-  ),
-
-  [_ADJUST] = KEYMAP(
-  //,--------+--------+--------+--------+--------+--------.                          ,--------+--------+--------+--------+--------+--------.
-      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                          |--------+--------+--------+--------+--------+--------|
-      RESET  , DEBUG  , RGB_HUD, RGB_SAD, RGB_VAD, _______,                            _______, _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------+--------.        ,--------|--------+--------+--------+--------+--------+--------|
-      BL_STEP, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
-  //`--------+--------+--------+----+---+--------+--------+--------/        \--------+--------+--------+---+----+--------+--------+--------'
-                                      _______, _______, _______,                  _______, _______, _______
-  //                                `--------+--------+--------'                `--------+--------+--------'
   )
 
 };
 
+/*
 #ifdef AUDIO_ENABLE
 float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
 #endif
+*/
 
 void persistent_default_layer_set(uint16_t default_layer) {
   eeconfig_update_default_layer(default_layer);
   default_layer_set(default_layer);
 }
 
+/*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
@@ -194,3 +177,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+*/
+
