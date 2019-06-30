bc3ed8a080db87fea2ed05aecc8e64b11040f6db
Akihiro Takizawa <akihiro.takizawa@gmail.com>
2019-06-30 21:53:09 +0900

    update foobar keymap

--- keyboards/40percentclub/foobar/keymaps/turky/keymap.c
+++ keyboards/40percentclub/foobar/keymaps/turky/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "keymap_jp.h"
 
 enum foobar_layers {
   QWERTY,
@@ -36,6 +37,71 @@ enum foobar_layers {
 #define LSFT_BSP LSFT_T(KC_BSPC)
 #define RGUI_ESC RGUI_T(KC_ESC)
 
+#define EXLM   JP_EXLM
+#define DQT    JP_DQT
+#define HASH   JP_HASH
+#define DLR    JP_DLR
+#define PERC   JP_PERC
+#define AMPR   JP_AMPR
+#define QUOT   JP_QUOT
+#define DQT    JP_DQT
+#define GRV    JP_GRV
+#define PIPE   JP_PIPE
+#define YEN    JP_YEN
+#define AT     JP_AT
+#define LCBR   JP_LCBR
+#define RCBR   JP_RCBR
+#define UNDS   JP_UNDS
+#define TILD   JP_TILD
+#define EQL    JP_EQL
+#define COLN   JP_COLN
+#define SLSH   JP_SLSH
+#define ASTR   JP_ASTR
+#define LPRN   JP_LPRN
+#define RPRN   JP_RPRN
+#define BSLS   JP_BSLS
+#define CIRC   JP_CIRC
+#define MINS   JP_MINS
+#define SCLN   JP_SCLN
+#define COMM   JP_COMM
+#define DOT    JP_DOT
+#define PLUS   JP_PLUS
+#define LBRC   JP_LBRC
+#define RBRC   JP_RBRC
+#define GUI    KC_LGUI
+#define ZHTG   JP_ZHTG
+#define ALT    KC_LALT
+#define CTL    KC_LCTL
+#define SFT    KC_LSFT
+#define ESC    KC_ESC
+#define TAB    KC_TAB
+#define SPC    KC_SPC
+#define BSPC   KC_BSPC
+#define HENK   KC_HENK
+#define MHEN   KC_MHEN
+#define HOME   KC_HOME
+#define END    KC_END
+#define DEL    KC_DEL
+#define WH_D   KC_WH_D
+#define MS_U   KC_MS_U
+#define WH_U   KC_WH_U
+#define PGDN   KC_PGDN
+#define PGUP   KC_PGUP
+#define MS_U   KC_MS_U
+#define MS_L   KC_MS_L
+#define MS_D   KC_MS_D
+#define MS_R   KC_MS_R
+#define KLEFT  KC_LEFT
+#define KUP    KC_UP
+#define KDOWN  KC_DOWN
+#define KRIGHT KC_RIGHT
+#define MUTE   KC_MUTE
+#define VOLD   KC_VOLD
+#define VOLU   KC_VOLU
+#define BTN2   KC_BTN2
+#define BTN1   KC_BTN1
+#define CTGUI  LCTL(KC_LGUI)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWERTY] = LAYOUT_split(
     KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
@@ -45,26 +111,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [FN1] = LAYOUT_split(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______
+    EXLM,    DQT,     HASH,    DLR,     PERC,    AMPR,    QUOT,    LPRN,    RPRN,    MINS,
+    _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, _______
   ),
 
   [FN2] = LAYOUT_split(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, KC_GRV,
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______,  TILD,   KC_GRV,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [FN3] = LAYOUT_split(
-    _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+       ESC, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     KC_TAB,  _______, _______, _______, _______, KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN, KC_QUOT,
-    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+     CTGUI, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   [FN4] = LAYOUT_split(
     _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
     KC_TAB,  _______, _______, _______, _______, KC_LABK, KC_RABK, KC_QUES, KC_COLN, KC_DQUO,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+       CTL, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [FN5] = LAYOUT_split(
