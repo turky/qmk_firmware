b9aac250144e114808bc40d0655565e83a21d832
Akihiro Takizawa <akihiro.takizawa@gmail.com>
2020-09-16 10:54:31 +0900

    update keymaps

--- keyboards/minidox/keymaps/turky/keymap.c
+++ keyboards/minidox/keymaps/turky/keymap.c
@@ -91,6 +91,7 @@ enum custom_keycodes {
 #define BTN1   KC_BTN1
 
 #define SFTZ   LSFT_T(KC_Z)
+#define GUIX   LGUI_T(KC_X)
 
 // Defines for task manager and such
 #define CALTDEL LCTL(LALT(KC_DEL))
@@ -117,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT( \
       KC_Q,  KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P,
       KC_A,  KC_S, KC_D, KC_F, KC_G,         KC_H, KC_J, KC_K, KC_L, SCLN,
-      SFTZ,  KC_X, KC_C, KC_V, KC_B,         KC_N, KC_M, COMM, DOT,  SLSH,
+      SFTZ,  GUIX, KC_C, KC_V, KC_B,         KC_N, KC_M, COMM, DOT,  SLSH,
                     SFT,  CTL,  SPC,         KC_ENT,RAISE,LOWER
 ),
 
