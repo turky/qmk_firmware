d63a0c1b2129986fdd22c8ec60309be3678fc00e
Akihiro Takizawa <akihiro.takizaw@gmail.com>
2022-08-02 10:25:10 +0900

    update

--- keyboards/maple_computing/minidox/keymaps/turky/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/turky/keymap.c
@@ -102,6 +102,7 @@ enum {
 #define CALTDEL LCTL(LALT(KC_DEL))
 #define TSKMGR LCTL(LSFT(KC_ESC))
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -121,10 +122,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT( \
-  TD_Q_ESC,  KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P,
-      KC_A,  KC_S, KC_D, KC_F, KC_G,         KC_H, KC_J, KC_K, KC_L, SCLN,
-      SFTZ,  GUIX, KC_C, KC_V, KC_B,         KC_N, KC_M, COMM, DOT,  SLSH,
-                    SFT,  CTL,  SPC,         KC_ENT,RAISE,LOWER
+TD(TD_Q_ESC),  KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P,
+        KC_A,  KC_S, KC_D, KC_F, KC_G,         KC_H, KC_J, KC_K, KC_L, SCLN,
+        SFTZ,  GUIX, KC_C, KC_V, KC_B,         KC_N, KC_M, COMM, DOT,  SLSH,
+                      SFT,  CTL,  SPC,         KC_ENT,RAISE,LOWER
 ),
 
 /* Raise
@@ -241,6 +242,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
- qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, ESC)
- };
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC)
+};
